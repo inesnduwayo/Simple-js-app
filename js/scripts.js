b595dba600318970b7fc6f40aefab9774f09fed8
inesnduwayo <154829231+inesnduwayo@users.noreply.github.com>
2024-02-24 17:36:50 -0600

    changed name of  for the second Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,1 @@
-alert ('Hello World');
-let favoriteFood= 'Pasta';
-document.write (favoriteFood);
-
+let pokemonList = [{name:"Bulbasaur", height:"7", types:["grass","poison"] },{name:"Jigglypuff", height:"5", types:["fairy","normal"]},{name:"Pidgeotto", height:"11", type:["flying","normal"]}];
