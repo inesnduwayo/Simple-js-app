f28f8c457d2b112e56998c3ee25ed4654bdff88a
inesnduwayo <154829231+inesnduwayo@users.noreply.github.com>
2024-02-24 17:37:34 -0600

    chenged the type of the second pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,3 @@
-let pokemonList = [{name:"Bulbasaur", height:"7", types:["grass","poison"] },{name:"Jigglypuff", height:"5", types:["fairy","normal"]},{name:"Pidgeotto", height:"11", type:["flying","normal"]}];
+let pokemonList = [{name:"Bulbasaur", height:"7", types:["grass","poison"] },
+{name:"Jigglypuff", height:"5", types:["fairy","normal"]},{name:"Pidgeotto", height:"11", type:["flying","normal"]}];
+
