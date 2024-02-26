c1b2fd92cb720fc9a9bf073116e6a2edfb51b633
inesnduwayo <154829231+inesnduwayo@users.noreply.github.com>
2024-02-26 13:49:37 -0600

    renamed type property to types

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,2 @@
-let pokemonList = [{name:"Bulbasaur", height:"7", types:["grass","poison"] },{name:"Jigglypuff", height:"5", type:["fairy","normal"]},{name:"Pidgeotto", height:"11", type:["flying","normal"]}];
+let pokemonList = [{name:"Bulbasaur", height:"7", types:["grass","poison"] },{name:"Jigglypuff", height:"5", types:["fairy","normal"]},{name:"Pidgeotto", height:"11", types:["flying","normal"]}];
 
