dd338f63c966d8b11008f687e94e64f9628aff3b
inesnduwayo <154829231+inesnduwayo@users.noreply.github.com>
2024-02-26 22:23:40 -0600

    added quotation marks in the document.write function

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
 let pokemonList = [
-{name:"Bulbasaur", height:"7", types:["grass","poison"] },
-{name:"Jigglypuff", height:"5", types:["fairy","normal"]},
-{name:"Pidgeotto", height:"11", types:["flying","normal"]}
-];
+ {name:"Bulbasaur", height:"7", types:["grass","poison"] },
+ {name:"Jigglypuff", height:"5", types:["fairy","normal"]},
+ {name:"Pidgeotto", height:"11", types:["flying","normal"]}
+ ];
 
-for (let i=0; i < pokemonList.length; i++){
+for (let i =0; i< pokemonList.length; i++){
     if(pokemonList[i].height <10 && pokemonList[i].height >6){
-      console.log(pokemonList[i].document.write() + "is average" );
+     console.log(pokemonList[i].document.write("Bulbasaur") + "is average");
     }else if (pokemonList[i].height <6){
-      console.log(pokemonlist[i].document.write() + "is smaller");
+      console.log(pokemonList[i].document.write("Jigglypuff") + "is smaller");
     }else {
-      console.log(pokemonList[i].document.write()+ "is big");
+      console.log(pokemonList[i].document.write("Pidgeotto") +  "is bigger");
     }
 
     }
+
+
