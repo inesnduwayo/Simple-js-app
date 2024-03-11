89d1aa9f05d4b605dfe0d4f11e416f471e4f95cf
inesnduwayo <154829231+inesnduwayo@users.noreply.github.com>
2024-03-11 15:28:05 -0500

    updated the scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,41 @@ let pokemonList = [
  pokemonList.forEach ( item => console.log(item));
 
  
+let pokemonRepository = (function (){
+   
+  let pokemonList = [
+    {
+      name: 'Bulbasaur',
+      height: 7,
+      types: ['grass', 'poison']
+    },
+    {
+      name: 'Jigglypuff',
+      height: 5,
+      types: ['fairy', 'normal']
+    },
+    {
+      name:'Pidgeotto',
+      height: 11,
+      types: ['flying', 'normal']
+    }
+  ]
+   function getAll () {
+      return pokemonList;
+   }
+   function add (pokemon){
+      pokemonList.push (pokemon);
+   }
+   
+   return {
+        getAll: getAll,
+        add: add
+   }
 
 
+})()
+
+console.log (pokemonRepository.getAll())
   
 
 
